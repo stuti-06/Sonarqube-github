18d2cb56e1b6912731650235169c7e99f7acd6d7
stuti-06 <stuti.nigam@capgemini.com>
2025-01-22 17:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-from flask import Flask
+def hello_world():
+ print("hello world")
  
-app = Flask(__name__)
- 
- 
-@app.route('/')
-def home():
-    """
-    home fun
-    """
-    return "<h1>Welcome to Home page<h1/>"
- 
- 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-has context menu
